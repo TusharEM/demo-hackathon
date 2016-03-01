e48b0438f116faa6cd36d95f03a03113722b5dbc
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-01 18:19:29 +0530

    Added index and text files

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+/*require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
+});
+
+$app->run();*/
+
+ini_set(error_reporting,E_ALL);
+ini_set("display_errors",1);
+
+print "Hello Demo Heoku!!!";
+
+# $file = fopen("test.txt","w");
+# print fwrite($file, json_encode($_POST));
+# fclose($file);
