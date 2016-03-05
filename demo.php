fd082bdc6b408e1ee29d1874d2b20952a5f2349a
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-05 13:30:40 +0530

    Write response in text file

--- demo.php
+++ demo.php
@@ -2,7 +2,10 @@
 ini_set(error_reporting,E_ALL);
 ini_set("display_errors",1);
 print "Here In";
-$request_body = file_get_contents('php://input');
-print "<pre>";
-print_r($request_body);
+
+$input = file_get_contents("php://input");
+$filename = 'test.txt';
+$filehandle = fopen($filename, 'w');
+fwrite($filehandle, $input);
+fclose($filehandle);
 ?>
