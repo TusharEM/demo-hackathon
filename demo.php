307f0b9d8475d8d37fa5b03fce7d6a04ed1897f8
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-05 12:57:25 +0530

    To grab gitHub hook response

--- demo.php
+++ demo.php
@@ -1,7 +1,7 @@
 <?php
-// ini_set(error_reporting,E_ALL);
-// ini_set("display_errors",1);
-
+ini_set(error_reporting,E_ALL);
+ini_set("display_errors",1);
+print "Here In"
 $request_body = file_get_contents('php://input');
 print "<pre>";
 print_r($request_body);
