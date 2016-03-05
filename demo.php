8504e00ea905bb440657af05bb228d63ff4cfee8
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-05 17:46:38 +0530

    executing git command using exec

--- demo.php
+++ demo.php
@@ -2,10 +2,9 @@
 ini_set(error_reporting,E_ALL);
 ini_set("display_errors",1);
 print "Here In";
-
-$input = file_get_contents("php://input");
-$filename = 'test.txt';
-$filehandle = fopen($filename, 'w');
-fwrite($filehandle, $input);
-fclose($filehandle);
+// $path = "/var/www/html/repos"; 
+// $a='';
+// chdir($path);
+print exec("whoami");
+print exec("git clone https://github.com/TusharEM/demo-hackathon.git");
 ?>
