60f5646d5765bc092f1a773dad296da66551baea
tusharm-cuelogic <tushar.mate@cuelogic.co.in>
2016-03-05 18:07:03 +0530

    Committed some changes

--- demo.php
+++ demo.php
@@ -3,8 +3,8 @@ ini_set(error_reporting,E_ALL);
 ini_set("display_errors",1);
 print "Here In";
 // $path = "/var/www/html/repos"; 
-// $a='';
 // chdir($path);
 print exec("whoami");
 print exec("git clone https://github.com/TusharEM/demo-hackathon.git");
+print "After Exec command";
 ?>
